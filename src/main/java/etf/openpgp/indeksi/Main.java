b9edc6904c959d5f694aa7fab31cbf78f06d2677
Proka-DESKTOP <prokic.milan@gmail.com>
2020-07-22 00:14:30 +0200

    generisanje para kljuceva

--- src/main/java/etf/openpgp/indeksi/Main.java
+++ src/main/java/etf/openpgp/indeksi/Main.java
@@ -1,8 +1,22 @@
 package etf.openpgp.indeksi;
 
+import etf.openpgp.indeksi.crypto.RSAKeyRing;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openpgp.PGPEncryptedData;
+import org.bouncycastle.openpgp.PGPException;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
 public class Main {
- 
-    public static void main(String[] args) {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static void main(String[] args) throws PGPException {
+        RSAKeyRing.printSecretKeyRing();
         App.app();
     }
 
