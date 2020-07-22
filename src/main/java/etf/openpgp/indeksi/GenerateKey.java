ebcf7299f850166f8eb3bd24df093afb8feac03c
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-07-22 22:54:58 +0200

    Add all logic to frontend part of generate key

--- src/main/java/etf/openpgp/indeksi/GenerateKey.java
+++ src/main/java/etf/openpgp/indeksi/GenerateKey.java
@@ -1,10 +1,16 @@
 package etf.openpgp.indeksi;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -14,6 +20,9 @@ import javafx.scene.text.Text;
 public class GenerateKey {
 	
 	private static VBox generateKeyVBox;
+	
+	private static String name, email;
+	private static int keySize;
 	
 	public static VBox openAddKeyMenu(BorderPane pane) {
 		createVBox(pane);
@@ -29,14 +38,55 @@ public class GenerateKey {
 	    title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 	    generateKeyVBox.getChildren().add(title);
 	     
-	    Label name = new Label("Enter name:");
-	    generateKeyVBox.getChildren().add(name);
+	    Label nameLabel = new Label("Enter name:");
+	    generateKeyVBox.getChildren().add(nameLabel);
+	    
+	    TextField nameField = new TextField();
+	    generateKeyVBox.getChildren().add(nameField);
 	     
 	    Label mail = new Label("Enter mail:");
 	    generateKeyVBox.getChildren().add(mail);
+	    
+	    TextField mailField = new TextField();
+	    generateKeyVBox.getChildren().add(mailField);
+	    
+	    Label keySizeLabel = new Label("Choose key size:");
+	    generateKeyVBox.getChildren().add(keySizeLabel);
+	    
+	    ObservableList<String> options = 
+	    	    FXCollections.observableArrayList(
+	    	        "1024",
+	    	        "2048",
+	    	        "4096"
+	    	    );
+	    final ComboBox comboBox = new ComboBox(options);
+	    comboBox.setValue("1024");
+	    generateKeyVBox.getChildren().add(comboBox);
 	     
 	    Button generateKey = new Button("GENERATE");
 	    generateKeyVBox.getChildren().add(generateKey);
+	    
+	    generateKey.setOnAction(new EventHandler<ActionEvent>() {
+	        public void handle(ActionEvent e) {
+	            email = mailField.getText();
+	            name = nameField.getText();
+	            keySize = Integer.parseInt(((String) comboBox.getValue()));
+	            
+	            boolean everythingOK = true;
+	           
+	            if(name.length() == 0) {
+	            	everythingOK = false;
+	            }
+	            
+	            if(email.length() == 0) {
+	            	everythingOK = false;
+	            }
+	            
+	            if(everythingOK) {
+	            	System.out.print(keySize);
+	            }
+	        }
+	    });
 	    
 	    Button cancel = new Button("CANCEL");
 	    cancel.setOnAction(new EventHandler<ActionEvent>() {
